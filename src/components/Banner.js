ce0b40e112fc0a06df58cc8a0141939b2fe90b2f
AlejandroGuerra1823 <100006307+AlejandroGuerra1823@users.noreply.github.com>
2022-06-18 11:52:56 -0500

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+///import logo192 from './../logo192.png';
+
+function Banner(prp){
+    return(
+        <div className="container">
+            <footer>
+  <ul>
+    <li><a href="/acerca-de">Acerca de</a></li>
+    <li><a href="/conectar">Conectar</a></li>
+    <li><a href="/rrss">Redes Sociales</a></li>
+  </ul>
+</footer>
+        </div>
+    );
+}
+
+export default Banner;
